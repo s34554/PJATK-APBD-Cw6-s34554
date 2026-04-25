0bb7ede7753eeb9f482150fce74b5389f0184674
Szymon Bak <s34554@pjwskt.edu.pl>
2026-04-25 17:30:48 +0200

    Add Http GET

--- WebApplication1/WebApplication1/Controllers/AppointmentsController.cs
+++ WebApplication1/WebApplication1/Controllers/AppointmentsController.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers;
 
-public class AppointmentsController : ControllerBase
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentsController(AppointmentService service) : ControllerBase
 {
-    
+    [HttpGet]
+    public async Task<ActionResult<List<AppointmentListDto>>> GetAll()
+    {
+        var appointments = await service.GetAllAsync();
+        return Ok(appointments);
+    }
 }
---
+++ WebApplication1/WebApplication1/Services/AppointmentService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using WebApplication1.DTOs;
+
+namespace WebApplication1.Services;
+
+public class AppointmentService(IConfiguration configuration)
+{
+    private readonly string _connectionString = configuration.GetConnectionString("DefaultConnection")
+                                                ?? throw new InvalidOperationException("Missing DefaultConnection.");
+
+    public async Task<List<AppointmentListDto>> GetAllAsync()
+    {
+        var result = new List<AppointmentListDto>();
+
+        const string sql = """
+                           SELECT
+                               a.IdAppointment,
+                               a.AppointmentDate,
+                               a.Status,
+                               a.Reason,
+                               p.FirstName + N' ' + p.LastName AS PatientFullName,
+                               p.Email AS PatientEmail
+                           FROM dbo.Appointments a
+                           JOIN dbo.Patients p ON p.IdPatient = a.IdPatient
+                           ORDER BY a.AppointmentDate;
+                           """;
+        await using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        await using var command = new SqlCommand(sql, connection);
+        await using var reader = await command.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            result.Add(new AppointmentListDto
+            {
+                IdAppointment = reader.GetInt32(0),
+                AppointmentDate = reader.GetDateTime(1),
+                Status = reader.GetString(2),
+                Reason = reader.GetString(3),
+                PatientFullName = reader.GetString(4),
+                PatientEmail = reader.GetString(5)
+            });
+        }
+        return result;
+    }
+}
